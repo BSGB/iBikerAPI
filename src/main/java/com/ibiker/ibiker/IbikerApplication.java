c31225c9edb953b67b4795f30854a76de80a1fe9
BSGB <puntlik@outlook.com>
2019-04-08 08:51:56 +0200

    First commit

---
+++ src/main/java/com/ibiker/ibiker/IbikerApplication.java
@@ -0,0 +1,13 @@
+package com.ibiker.ibiker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbikerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbikerApplication.class, args);
+	}
+
+}
